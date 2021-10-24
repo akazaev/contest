cb13aa93c04a84897be14ce5528c9ac12534f80e
a.kazaev <a.kazaev@tnxholding.com>
2021-10-24 13:45:12 +0300

    bugfix.

--- create_db.sh
+++ create_db.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # remove old db
 rm documents.db
 
--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # setup libraries
-sudo apt-get install libreoffice tesseract-ocr tesseract-ocr-rus
+sudo apt-get install libreoffice tesseract-ocr tesseract-ocr-rus sqlite3
 pip3 install -r requirements.txt
 python3 -m spacy download ru_core_news_sm
 python3 -m spacy download ru_core_news_lg
